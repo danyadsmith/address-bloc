1685fc8036a59639f6938b743a52f1835e08c20b
Danya D. Smith <danyadsmith@gmail.com>
2015-09-22 21:14:24 -0400

    Allows users to search by name and utilize search submenu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -150,7 +150,49 @@ class MenuController
   end
 
   def search_entries
+
+    print "Search by name: "
+    name = gets.chomp
+
+    match = @address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
+
   end
+
+  def search_submenu(entry)
+
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+    case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
+  end    
 
   def read_csv
 
